659f575f9ddd609d7a3bb22f104fff6349d01656
Yashaswi <y.maharshi@gmail.com>
2017-05-17 16:10:03 +0530

    Fresh commit with Webpack dev server , Babel for transpilation, React for UI

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    entry: "./index.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js",
+        publicPath: "/"
+    }, module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: "babel-loader",
+            include: __dirname,
+            query: {
+                presets: ['es2015', 'react', 'react-hmre']
+            }
+        }]
+    }
+}
